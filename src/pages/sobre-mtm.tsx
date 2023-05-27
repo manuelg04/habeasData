a67375929941d2e2dc6c0f1583baf92123f08c25
Manuel Gonzalez <manuelg9704@gmail.com>
2023-05-27 10:02:20 -0500

    mas estilos

--- src/pages/sobre-mtm.tsx
+++ src/pages/sobre-mtm.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable max-len */
 // SobreMTM.tsx
 import {
-  Typography, Card, Row, Col,
+  Typography, Collapse,
 } from 'antd';
-import Image from 'next/image';
 
 const { Title, Paragraph } = Typography;
+const { Panel } = Collapse;
 
 const SobreMTM = () => (
   <div style={{
@@ -14,104 +14,58 @@ const SobreMTM = () => (
     height: '100vh',
   }}
   >
-    <Card style={{
-      marginBottom: '1em',
-      backgroundColor: '#ffffff',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-    }}
+    <Title style={{ color: '#003a8c' }}>Sobre MTM</Title>
+    <Collapse
+      defaultActiveKey={['1']}
+      style={{
+        marginTop: '1em',
+        backgroundColor: '#ffffff',
+        borderRadius: '8px',
+        boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+      }}
     >
-      <Title level={2}>MISIÓN</Title>
-      <Row align="middle" justify="space-between">
-        <Col xs={24} sm={24} md={10} lg={10} xl={8}>
-          <Image src="/Mision.jpg" alt="Misión" width={500} height={300} />
-        </Col>
-        <Col xs={24} sm={24} md={14} lg={14} xl={16} style={{ paddingLeft: '2em' }}>
-          <Paragraph style={{
-            backgroundColor: 'white',
-            fontSize: '20px',
-            lineHeight: '1.5',
-          }}
-          >
-            Somos una empresa dedicada a generar servicio en la modalidad de transporte de carga terrestre...
-          </Paragraph>
-        </Col>
-      </Row>
-    </Card>
+      <Panel header="MISIÓN" key="1">
+        <Paragraph style={{
+          fontSize: '20px',
+          lineHeight: '1.5',
+        }}
+        >
+          Somos una empresa dedicada a generar servicio en la modalidad de transporte de carga terrestre...
+        </Paragraph>
+      </Panel>
 
-    <Card style={{
-      marginBottom: '1em',
-      backgroundColor: '#ffffff',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-    }}
-    >
-      <Title level={2}>VISION</Title>
-      <Row align="middle" justify="space-between">
-        <Col xs={24} sm={24} md={10} lg={10} xl={8}>
-          <Image src="/Mision.jpg" alt="Misión" width={500} height={300} />
-        </Col>
-        <Col xs={24} sm={24} md={14} lg={14} xl={16} style={{ paddingLeft: '2em' }}>
-          <Paragraph style={{
-            backgroundColor: 'white',
-            fontSize: '20px',
-            lineHeight: '1.5',
-          }}
-          >
-            Somos una empresa dedicada a generar servicio en la modalidad de transporte de carga terrestre...
-          </Paragraph>
-        </Col>
-      </Row>
-    </Card>
-    <Card style={{
-      marginBottom: '1em',
-      backgroundColor: '#ffffff',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-    }}
-    >
-      <Title level={2}>POLITICA DE SEGURIDAD</Title>
-      <Row align="middle" justify="space-between">
-        <Col xs={24} sm={24} md={10} lg={10} xl={8}>
-          <Image src="/Mision.jpg" alt="Misión" width={500} height={300} />
-        </Col>
-        <Col xs={24} sm={24} md={14} lg={14} xl={16} style={{ paddingLeft: '2em' }}>
-          <Paragraph style={{
-            backgroundColor: 'white',
-            fontSize: '20px',
-            lineHeight: '1.5',
-          }}
-          >
-            Somos una empresa dedicada a generar servicio en la modalidad de transporte de carga terrestre...
-          </Paragraph>
-        </Col>
-      </Row>
-    </Card>
-    <Card style={{
-      marginBottom: '1em',
-      backgroundColor: '#ffffff',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-    }}
-    >
-      <Title level={2}>VALORES</Title>
-      <Row align="middle" justify="space-between">
-        <Col xs={24} sm={24} md={10} lg={10} xl={8}>
-          <Image src="/Mision.jpg" alt="Misión" width={500} height={300} />
-        </Col>
-        <Col xs={24} sm={24} md={14} lg={14} xl={16} style={{ paddingLeft: '2em' }}>
-          <Paragraph style={{
-            backgroundColor: 'white',
-            fontSize: '20px',
-            lineHeight: '1.5',
-          }}
-          >
-            Somos una empresa dedicada a generar servicio en la modalidad de transporte de carga terrestre...
-          </Paragraph>
-        </Col>
-      </Row>
-    </Card>
-    {/* El resto de las Cards siguen el mismo estilo... */}
+      <Panel header="VISIÓN" key="2">
+        <Paragraph style={{
+          fontSize: '20px',
+          lineHeight: '1.5',
+        }}
+        >
+          Somos una empresa dedicada a generar servicio en la modalidad de transporte de carga terrestre...
+        </Paragraph>
+      </Panel>
+
+      <Panel header="POLÍTICA DE SEGURIDAD" key="3">
+        <Paragraph style={{
+          fontSize: '20px',
+          lineHeight: '1.5',
+        }}
+        >
+          Somos una empresa dedicada a generar servicio en la modalidad de transporte de carga terrestre...
+        </Paragraph>
+      </Panel>
+
+      <Panel header="VALORES" key="4">
+        <Paragraph style={{
+          fontSize: '20px',
+          lineHeight: '1.5',
+        }}
+        >
+          Somos una empresa dedicada a generar servicio en la modalidad de transporte de carga terrestre...
+        </Paragraph>
+      </Panel>
+
+      {/* Puedes agregar más Panels si lo necesitas... */}
+    </Collapse>
   </div>
 );
 
