b623e61f834356a501cf4141ecb2f755652f2a4f
Manuel Gonzalez <manuelg9704@gmail.com>
2023-06-01 17:46:07 -0500

    quitando el footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import Navbar from '../../components/navBar';
 import PlantillaEmail from '../emails/plantillaEmail';
-import MyFooter from '../../components/footer';
 
 function MyApp({ Component, pageProps }) {
   return (
